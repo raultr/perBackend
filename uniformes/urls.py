c41cf717d8b9882fdad02076dab77bb74e523a75
raultr <raultr@gmail.com>
2016-10-15 01:21:02 -0500

    la ruta del post estaba mal para los uniformes

--- uniformes/urls.py
+++ uniformes/urls.py
@@ -4,5 +4,5 @@ from uniformes import views
 
 urlpatterns = patterns('uniformes.views',
  	 url(r'^uniforme/$',views.UniformeConDetallesLista.as_view(), name='uniforme_post_view'),
- 	 url(r'^unifore/personal/$',views.PersonalUniformeConDetalles.as_view(), name='personal_uniforme_view'),
+ 	 url(r'^uniforme/personal/$',views.PersonalUniformeConDetalles.as_view(), name='personal_uniforme_view'),
 	)
